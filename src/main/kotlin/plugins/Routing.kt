e6865dab3f453d836c0202c4a86e2d08ed3979cf
Ahmed <ahmedbobos.rashid@gmail.com>
2025-09-21 01:29:36 +0300

    Implement alternative hero retrieval route and repository interface

--- src/main/kotlin/plugins/Routing.kt
+++ src/main/kotlin/plugins/Routing.kt
@@ -1,6 +1,7 @@
 package com.example.plugins
 
 import com.example.routes.getAllHeroes
+import com.example.routes.getAllHeroesAlternative
 import com.example.routes.root
 import com.example.routes.searchHeroes
 import io.ktor.server.application.*
@@ -13,7 +14,8 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         root()
-        getAllHeroes()
+        //getAllHeroes()
+        getAllHeroesAlternative()
         staticResources("/images", "images")
         searchHeroes()
 
---
+++ src/main/kotlin/routes/AllHeroesAlternative.kt
@@ -0,0 +1,43 @@
+package com.example.routes
+
+import com.example.models.ApiResponse
+import com.example.repository.HeroRepositoryAlternative
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
+
+fun Route.getAllHeroesAlternative() {
+    val heroRepositoryAlternative: HeroRepositoryAlternative by application.inject()
+
+    get("/anime/heroes") {
+        try {
+            val page = call.request.queryParameters["page"]?.toInt() ?: 1
+            val limit = call.request.queryParameters["limit"]?.toInt() ?: 20
+
+            val apiResponse = heroRepositoryAlternative.getAllHeroes(
+                page = page, limit = limit
+            )
+            call.respond(
+                message = apiResponse,
+                status = HttpStatusCode.OK
+            )
+        } catch (e: NumberFormatException) {
+            call.respond(
+                message = ApiResponse(success = false, message = "Only Numbers Allowed."),
+                status = HttpStatusCode.BadRequest
+            )
+        } catch (e: IllegalArgumentException) {
+            call.respond(
+                message = ApiResponse(success = false, message = "Heroes not Found."),
+                status = HttpStatusCode.NotFound
+            )
+        } catch (e: Exception) {
+            call.respond(
+                message = ApiResponse(success = false, message = e.stackTrace.toString()),
+                status = HttpStatusCode.NotFound
+            )
+        }
+    }
+}
